e903b6a04b72813f9332b61d55be868dc20ea4b8
jeffreyjas <jaspreetsingh021103@gmail.com>
2024-09-07 17:11:44 +1000

    Added styling for router and dashboard

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from "react";
 import "../style.css"
-import { Outlet, Link, useMatch } from "react-router-dom";
+import {Outlet, Link, useLocation} from "react-router-dom";
 
 
 const Sidebar = () => {
-    const isDashboard = useMatch('dashboard');
-    const isEducation = useMatch('education');
-
+    const location = useLocation();
     return (
         <>
-            <div>
-                <h1>Sidebar is here</h1>
-                <h3>(i.e. this is where the links are, we can transform this so it's on the left of the page)</h3>
-                <Link to={`dashboard`} className={isDashboard ? "sidebar-active" : ""}>Click to show Dashboard!</Link><br/>
-                <Link to={`education`} className={isEducation ? "sidebar-active" : ""}>Click to show Education!</Link>
-
-            </div>
-            <hr/>
-            <div>
-                <h1>The page selected is output here</h1>
-                <h3>(Dashboard by default)</h3>
-                <Outlet />
+            <div className="flexbox-container">
+                <div className="sidebar-container">
+                    <div className="icon-holder">
+                        <img src="../images/hamburger-icon.png" alt="menu-icon"></img>
+                        <br/>
+                    </div>
+                    <div className="arrow-icon-container">
+                        <div className="icon-holder">
+                            <Link to={`dashboard`} id="dashboard">
+                                <img src="../images/dashboard-icon.png" alt="dashboard-icon" id="document-icon"></img>
+                                <br/> {location.pathname === "/dashboard" ? <p>Dashboard &larr;</p> : <p>Dashboard</p>}
+                            </Link><br/>
+                        </div>
+                    </div>
+                    <div className="arrow-icon-container">
+                        <div className="icon-holder">
+                            <Link to={`education`} id="education">
+                                <img src="../images/education-icon.png" alt="education-icon" id="education-icon"></img>
+                                <br/> {location.pathname === "/education" ? <p>Education &larr;</p> : <p>Education</p>}
+                            </Link>
+                        </div>
+                    </div>
+                    <div id="button-holder">
+                        <p>Last Login Date:</p>
+                        <p>insert date here</p>
+                        <button id="logout-button" onClick={() => {console.log(location.pathname)}}>Log Out</button>
+                    </div>
+                </div>
+                <div className="outlet">
+                    <h1>The page selected is output here</h1>
+                    <h3>(Dashboard by default)</h3>
+                    <Outlet />
+                </div>
             </div>
         </>
     );
