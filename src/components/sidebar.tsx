11c859cf9c556a054e1f78f50751db33869db9ec
PoseidonOcto <octopus0246@gmail.com>
2024-09-07 16:31:44 +1000

    Added interface for detecting which tab is active on sidebar.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "../style.css"
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useMatch } from "react-router-dom";
 
 
 const Sidebar = () => {
+    const isDashboard = useMatch('dashboard');
+    const isEducation = useMatch('education');
+
     return (
         <>
             <div>
                 <h1>Sidebar is here</h1>
                 <h3>(i.e. this is where the links are, we can transform this so it's on the left of the page)</h3>
-                <Link to={`dashboard`}>Click to show Dashboard!</Link><br/>
-                <Link to={`education`}>Click to show Education!</Link>
+                <Link to={`dashboard`} className={isDashboard ? "sidebar-active" : ""}>Click to show Dashboard!</Link><br/>
+                <Link to={`education`} className={isEducation ? "sidebar-active" : ""}>Click to show Education!</Link>
 
             </div>
             <hr/>
