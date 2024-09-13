2956ff7ef92b69355182e287a64e4e32af438989
jeffreyjas <jaspreetsingh021103@gmail.com>
2024-09-13 10:14:15 +1000

    more styling yippee

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "../style.css"
 import {Outlet, Link, useLocation} from "react-router-dom";
+import set = chrome.cookies.set;
 
-
+// TODO Make side bar collapse on click on an icon
 const Sidebar = () => {
+    const [sideBarVisible, setSideBarClicked] = useState<boolean>(false);
     const location = useLocation();
     return (
         <>
             <div className="flexbox-container">
-                <div className="sidebar-container">
+                <div className={sideBarVisible ? "sidebar-container" : "collapsed-siderbar-container"}>
                     <div className="icon-holder">
-                        <img src="../images/hamburger-icon.png" alt="menu-icon"></img>
+                        <img src="../images/hamburger-icon.png" alt="menu-icon" onClick={() => setSideBarClicked(!sideBarVisible)}></img>
                         <br/>
                     </div>
                     <div className="arrow-icon-container">
                         <div className="icon-holder">
                             <Link to={`dashboard`} id="dashboard">
                                 <img src="../images/dashboard-icon.png" alt="dashboard-icon" id="document-icon"></img>
-                                <br/> {location.pathname === "/dashboard" ? <p><strong>Dashboard</strong></p> : <p>Dashboard</p>}
+                                <br/> {sideBarVisible ? (location.pathname === "/dashboard" ? <p><strong>Dashboard</strong></p> : <p>Dashboard</p>) : <p></p>}
                             </Link><br/>
                         </div>
                     </div>
@@ -25,7 +27,7 @@ const Sidebar = () => {
                         <div className="icon-holder">
                             <Link to={`education`} id="education">
                                 <img src="../images/education-icon.png" alt="education-icon" id="education-icon"></img>
-                                <br/> {location.pathname === "/education" ? <p><strong>Education</strong></p> : <p>Education</p>}
+                                <br/> {sideBarVisible ? (location.pathname === "/education" ? <p><strong>Education</strong></p> : <p>Education</p>) : <p></p>}
                             </Link>
                         </div>
                     </div>
@@ -33,14 +35,9 @@ const Sidebar = () => {
                         <div className="icon-holder">
                             <Link to={`preferences`} id="preferences">
                                 <img src="../images/gear-icon.png" alt="preferences-icon" id="preferences-icon"></img> {/* https://www.flaticon.com/free-icon/gear_1160356 */}
-                                <br/> {location.pathname === "/preferences" ? <p><strong>Preferences</strong></p> : <p>Preferences</p>}
+                                <br/> {sideBarVisible ? (location.pathname === "/preferences" ? <p><strong>Preferences</strong></p> : <p>Preferences</p>) : <p></p>}
                             </Link>
                         </div>
-                    </div>
-                    <div id="button-holder">
-                        <p>Last Login Date:</p>
-                        <p>insert date here</p>
-                        <button id="logout-button" onClick={() => {console.log(location.pathname)}}>Log Out</button>
                     </div>
                 </div>
                 <div className="outlet">
