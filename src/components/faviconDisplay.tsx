ea3aeb5ccb067f6496f24dbcf3bf52fb348295e7
PoseidonOcto <octopus0246@gmail.com>
2024-10-09 16:55:15 +1000

    Documented faviconDisplay.

--- src/components/faviconDisplay.tsx
+++ src/components/faviconDisplay.tsx
@@ -5,39 +5,19 @@ export const getFaviconOfWebsite = (url: string): string => {
     return `https://www.google.com/s2/favicons?sz=64&domain_url=${url}`
 }
 
+/*
+ * The image may fail to load and return a temporary image, with the website we
+ * query returning a replacement image but also a 404 error. This 404 error is
+ * logged to console. Because it returns a replacement image, the 'onerror'
+ * image property never triggers. Hence, catching this 404 error before it logs 
+ * is suprisingly hard.
+ */
 const FaviconDisplay = ( {url}: {url: string} ) => {
-    const [image, setImage] = useState<ReactElement | undefined>(undefined);
-
-    useEffect(() => {
-        setImage(React.createElement("img", {
-            src: getFaviconOfWebsite(url),
-            onError: ({ currentTarget }: any) => {
-                currentTarget.onerror = null; // prevents looping
-                // currentTarget.src="image_path_here";
-                console.log(currentTarget);
-                console.log("foo");
-                displayDefaultImage();
-            },
-        }));
-    }, []);
-
-    const displayDefaultImage = () => {
-        setImage(React.createElement("img", {src: "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://edition.cnn.com/&size=64"}));
-    }
-
     return (
         <>
-            {image === undefined && <div className="loadingIcon"></div>}
-            {image !== undefined && image}
+            <img src={getFaviconOfWebsite(url)}/>
         </>
     );
 };
 
-
-            // <img src={getFaviconOfWebsite(url)} 
-            //     onError={({ currentTarget }) => {
-            //         currentTarget.onerror = null; // prevents looping
-            //         // currentTarget.src="image_path_here";
-            //     }}
-            // />
 export default FaviconDisplay;
