b15889a1b4b35e8418297d5326571938bcb6a49d
jeffreyjas <jaspreetsingh021103@gmail.com>
2024-10-11 08:45:02 +1000

    added siderbar.css - moved styling to correct css files

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../style.css"
+import "./sidebar.css"
 import {Outlet, Link, useLocation} from "react-router-dom";
 import set = chrome.cookies.set;
 import {MessageMode} from "../types";
