c7c2f007535594864708352a05fba171b1fdf2dc
PoseidonOcto <octopus0246@gmail.com>
2024-10-16 18:04:34 +1000

    added youtube video explainer.

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "../style.css"
+import "./help.css"
 
 const Help = () => {
 
     return (
         <>
             <h1>Help</h1>
+            <h4>The following video walks you through our extension.</h4>
+            <br/>
+            <iframe src="https://www.youtube.com/embed/YNapXCUBqNI?si=l0YzZh_TDHvPye2x" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </>
     );
 };
